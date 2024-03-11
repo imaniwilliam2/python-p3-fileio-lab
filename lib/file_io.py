47881eb0a6eac3f5a20d586dbfdcbacedf1a2f0a
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-03-11 13:31:43 -0400

    tests passing

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name) + ".txt"
+
+    with open(file_name, mode="w") as file_name:
+        file_name.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name) + ".txt"
+
+    with open(file_name, mode="a") as file_name:
+        file_name.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name) + ".txt"
+
+    with open(file_name, mode="r") as file_name:
+        file_content = file_name.read()
+
+    return file_content
